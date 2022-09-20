7b32fd69afe318591c0411fceea51c139b42c19e
Kalin <Kalin@DESKTOP-529AING>
2022-09-21 01:44:27 +0300

    solution

---
+++ Stacks_and_Queues/src/Decimal_to_Binary_Converter.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Decimal_to_Binary_Converter {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		ArrayDeque<Integer> result = new ArrayDeque<>();
+		if (n == 0) {
+			System.out.println(0);
+			return;
+		}
+		while (n > 0) {
+			result.push(n % 2);
+			n /= 2;
+		}
+		result.forEach(x -> System.out.print(x));
+	}
+
+}
