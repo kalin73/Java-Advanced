49a6e1953b20ac728c1ccef99d4b44507573362c
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-09-29 18:09:47 +0300

    solution

--- Sets_And_Maps_Advanced/src/Sets/Parking_Lot.java
+++ Sets_And_Maps_Advanced/src/Sets/Parking_Lot.java
@@ -1,10 +1,30 @@
 package Sets;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
 public class Parking_Lot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		LinkedHashSet<String> licenseNumbs = new LinkedHashSet<>();
+		String[] input = sc.nextLine().split(", ");
+		
+		while (!input[0].equals("END")) {
 
+			if (input[0].equals("IN")) {
+				licenseNumbs.add(input[1]);
+			} else {
+				licenseNumbs.remove(input[1]);
+			}
+			input = sc.nextLine().split(", ");
+		}
+		if (licenseNumbs.isEmpty()) {
+			System.out.println("Parking Lot is Empty");
+		} else {
+			licenseNumbs.forEach(x -> System.out.println(x));
+		}
+		sc.close();
 	}
 
 }
