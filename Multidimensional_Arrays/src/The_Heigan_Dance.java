7a10e56e2a2caeeb7883bd20efe99e5e27a4919a
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-09-29 22:12:28 +0300

    solution

---
+++ Multidimensional_Arrays/src/The_Heigan_Dance.java
@@ -0,0 +1,9 @@
+
+public class The_Heigan_Dance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Sets_And_Maps_Advanced/src/Sets/Voina_Number_game.java
@@ -0,0 +1,17 @@
+package Sets;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class Voina_Number_game {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		LinkedHashSet<String> player1 =Arrays.stream(sc.nextLine().split(" ")).map(x->);
+		LinkedHashSet<String> player2 = new LinkedHashSet<>();
+		
+		player1
+	}
+
+}
