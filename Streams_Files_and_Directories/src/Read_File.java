a2c7f1881c458b48a098b966a0493e0178a714d7
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-10-09 01:09:21 +0300

    solution

---
+++ Streams_Files_and_Directories/src/Read_File.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Read_File {
+
+	public static void main(String[] args) throws IOException {
+		String path = "C:\\Users\\Kalin\\Desktop\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+		FileInputStream inputStream = new FileInputStream(path);
+		int n = inputStream.read();
+		while (n >= 0) {
+			System.out.print(Integer.toBinaryString(n) + " ");
+			n = inputStream.read();
+		}
+	}
+
+}
