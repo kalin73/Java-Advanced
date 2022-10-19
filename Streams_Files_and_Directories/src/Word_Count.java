079d32a350b90df5eee54e1a0331c370fcbb17ba
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-10-19 19:13:48 +0300

    solution

--- Streams_Files_and_Directories/src/Word_Count.java
+++ Streams_Files_and_Directories/src/Word_Count.java
@@ -2,57 +2,61 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Word_Count {
 
 	public static void main(String[] args) throws IOException {
-		String path = "C:\\Users\\Milen\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\words.txt";
-		Path path2 = Paths
-				.get("C:\\Users\\Milen\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\text.txt");
+		String path = "C:\\Users\\Kalin\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\words.txt";
+		String path2 = "C:\\Users\\Kalin\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\text.txt";
 		FileReader fr = new FileReader(path);
 		BufferedReader reader = new BufferedReader(fr);
-		PrintWriter writer = new PrintWriter("C:\\Users\\Milen\\Desktop\\result.txt");
+		PrintWriter writer = new PrintWriter("C:\\Users\\Kalin\\Desktop\\result.txt");
 		Map<String, Integer> count = new HashMap<>();
 
 		String line = reader.readLine();
-		List<String> lines = Files.readAllLines(path2);
-		String[] text; 
-for(int i=0;i<lines.size();i++) {
-	
-	
-	
-}
 
 		while (line != null) {
 
 			String[] words = line.split(" ");
+			FileReader fr2 = new FileReader(path2);
+			BufferedReader reader2 = new BufferedReader(fr2);
+			String line2 = reader2.readLine();
 
-			for (int i = 0; i < words.length; i++) {
-				String word = words[i];
-				count.putIfAbsent(word, 1);
+			while (line2 != null) {
+				String[] text = line2.split(" ");
+				for (int i = 0; i < words.length; i++) {
+					for (int k = 0; k < text.length; k++) {
+						String word = words[i];
 
-				if (lines.contains(word)) {
-					if (count.containsKey(word)) {
+						if (word.equals(text[k])) {
+							if (count.containsKey(word)) {
 
-						count.put(word, count.get(word) + 1);
+								count.put(word, count.get(word) + 1);
+
+							}
+							count.putIfAbsent(word, 1);
+						}
 
 					}
 				}
+				line2 = reader2.readLine();
 			}
 			line = reader.readLine();
+			reader2.close();
 		}
+
 		count.entrySet().stream().sorted((f, s) -> Integer.compare(s.getValue(), f.getValue())).forEach(x -> {
 
 			writer.printf("%s - %d%n", x.getKey(), x.getValue());
 			writer.flush();
 
 		});
+
+		reader.close();
+		writer.close();
+
 	}
 
 }
