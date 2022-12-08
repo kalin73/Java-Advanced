fb364589720a317906d7e8ad3e269589ee9be673
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-12-09 00:49:02 +0200

    better solution

--- Functional_Programming/src/Count_Uppercase_Words.java
+++ Functional_Programming/src/Count_Uppercase_Words.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Count_Uppercase_Words {
@@ -8,15 +11,29 @@ public class Count_Uppercase_Words {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		List<String> text = Arrays
-				.stream(sc.nextLine().split(" "))
-				.filter(x -> Character.isUpperCase(x.charAt(0)))
-				.collect(Collectors.toList());
+		List<String> text = Arrays.stream(sc.nextLine().split(" ")).collect(Collectors.toList());
+		text = filterUpperCase(text, x -> Character.isUpperCase(x.charAt(0)));
 
 		System.out.println(text.size());
-		text.stream().forEach(x -> System.out.println(x));
-		
+		print(text, System.out::println);
 		sc.close();
 	}
 
+	private static List<String> filterUpperCase(List<String> text, Predicate<String> predicate) {
+		List<String> upperCaseWords = new ArrayList<>();
+
+		for (String s : text) {
+			boolean isUpperCase = predicate.test(s);
+			if (isUpperCase) {
+				upperCaseWords.add(s);
+			}
+		}
+		return upperCaseWords;
+	}
+
+	private static void print(List<String> text, Consumer<String> consumer) {
+		for (String s : text) {
+			consumer.accept(s);
+		}
+	}
 }
