934eb28104b637b6eadc100f8e234fd895765dd4
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-01-15 18:56:27 +0200

    fix

--- Functional_Programming/src/Find_Evens_or_Odds.java
+++ Functional_Programming/src/Find_Evens_or_Odds.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-import java.util.function.Predicate;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
 
 public class Find_Evens_or_Odds {
 
@@ -7,26 +8,14 @@ public class Find_Evens_or_Odds {
 		Scanner sc = new Scanner(System.in);
 
 		String[] bounds = sc.nextLine().split(" ");
-		String condition = sc.nextLine();
-
-		if (condition.equals("even")) {
-			print(bounds, x -> x % 2 == 0);
-
-		} else {
-			print(bounds, x -> x % 2 != 0);
-
-		}
-		sc.close();
-	}
-
-	private static void print(String[] bounds, Predicate<Integer> predicate) {
 		int min = Integer.parseInt(bounds[0]);
 		int max = Integer.parseInt(bounds[1]);
 
-		for (int i = min; i <= max; i++) {
-			if (predicate.test(i)) {
-				System.out.print(i + " ");
-			}
-		}
+		String condition = sc.nextLine();
+
+		IntPredicate evenOddFilter = x -> "even".equals(condition) == (x % 2 == 0);
+		IntStream.rangeClosed(min, max).filter(evenOddFilter).forEach(x -> System.out.print(x + " "));
+
+		sc.close();
 	}
 }
