6182274c4f66a9d96362eedcb009981542e5925d
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-01-15 17:16:19 +0200

    fix

--- Functional_Programming/src/Add_VAT.java
+++ Functional_Programming/src/Add_VAT.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -9,18 +10,18 @@ public class Add_VAT {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		List<Double> numbers = Arrays.stream(sc.nextLine().split(", ")).map(x -> Double.parseDouble(x))
+		List<Double> numbers = Arrays.stream(sc.nextLine().split(", "))
+				.map(x -> Double.parseDouble(x))
 				.collect(Collectors.toList());
 
 		Function<Double, Double> function = x -> x * 1.2;
-		System.out.println("Prices with VAT:");
+		Consumer<List<Double>> consumer = list -> list.stream()
+				.map(function)
+				.forEach(x -> System.out.printf("%.2f%n", x));
 
-		numbers.forEach(x -> {
-			Double result = function.apply(x);
-			System.out.printf("%.2f%n", result);
-		});
+		System.out.println("Prices with VAT:");
+		consumer.accept(numbers);
 
 		sc.close();
-
 	}
 }
