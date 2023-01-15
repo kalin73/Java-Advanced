eb198d2eab4a9b38c508b6eb02a4266bd99b3349
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-01-15 14:56:32 +0200

    fix

--- Functional_Programming/src/Sum_Numbers.java
+++ Functional_Programming/src/Sum_Numbers.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Sum_Numbers {
@@ -8,13 +9,13 @@ public class Sum_Numbers {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		List<Integer> numbers = Arrays
-				.stream(sc.nextLine().split(", "))
-				.map(Integer::parseInt)
+		List<Integer> numbers = Arrays.stream(sc.nextLine().split(", ")).map(Integer::parseInt)
 				.collect(Collectors.toList());
 
-		int sum = numbers.stream().mapToInt(x -> x).sum();
-		System.out.println("Count = " + numbers.size() + "\nSum = " + sum);
+		Function<List<Integer>, String> count = x -> "Count = " + x.size();
+		Function<List<Integer>, String> sum = x -> "Sum = " + x.stream().mapToInt(Integer::intValue).sum();
+
+		System.out.println(count.apply(numbers) + "\n" + sum.apply(numbers));
 
 		sc.close();
 	}
